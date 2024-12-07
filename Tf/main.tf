2e10362deaa81812f8f40886daed9e1854cc144b
anu erics <anuerics@outlook.com>
2024-12-07 07:05:02 +0000

    Updated main.tf

--- Tf/main.tf
+++ Tf/main.tf
@@ -9,4 +9,5 @@ resource "azurerm_storage_account" "example" {
   location                 = data.azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
+}
 
