4e2e27f7be51846516e2341f6699e83e47a65f40
anu erics <anuerics@outlook.com>
2024-12-07 05:17:41 +0000

    Updated variables.tf

--- Tf/variables.tf
+++ Tf/variables.tf
@@ -3,11 +3,11 @@ variable "resource_group_name" {
   type        = string
 }
 
-variable "location" {
-  description = "The location of the resource group"
-  type        = string
+#variable "location" {
+  #description = "The location of the resource group"
+  #type        = string
   
-}
+#}
 
 variable "storage_account_name" {
   description = "The name of the storage account"
