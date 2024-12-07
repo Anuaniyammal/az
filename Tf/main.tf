038825214ff983bd8524438d4fa2ccb70784051e
anu erics <anuerics@outlook.com>
2024-12-07 07:04:48 +0000

    Updated main.tf

--- Tf/main.tf
+++ Tf/main.tf
@@ -3,14 +3,10 @@ data "azurerm_resource_group" "example" {
   #location = var.location
 }
 
-data "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "example" {
   name                     = var.storage_account_name
   resource_group_name      = data.azurerm_resource_group.example.name
-  #location                 = data.azurerm_resource_group.example.location
-  #account_tier             = "Standard"
- # account_replication_type = "GRS"
+  location                 = data.azurerm_resource_group.example.location
+  account_tier             = "Standard"
+  account_replication_type = "GRS"
 
-  #tags = {can't set the tags attribute in a data source block
-    #environment = var.environment
-  #}
-}#
