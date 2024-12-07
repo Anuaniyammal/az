07bbb7f68db65406a78f98c7e81c6d78584ed700
anu erics <anuerics@outlook.com>
2024-12-07 06:31:41 +0000

    Updated main.tf

--- Tf/main.tf
+++ Tf/main.tf
@@ -7,8 +7,8 @@ data "azurerm_storage_account" "example" {
   name                     = var.storage_account_name
   resource_group_name      = data.azurerm_resource_group.example.name
   #location                 = data.azurerm_resource_group.example.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+  #account_tier             = "Standard"
+ # account_replication_type = "GRS"
 
   tags = {
     environment = var.environment
