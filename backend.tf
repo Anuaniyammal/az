c56b09b47fd4aba66a412ea7ac7ead020b2de6a7
anu erics <anuerics@outlook.com>
2024-12-07 02:57:20 +0000

    Updated backend.tf

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
   resource_group_name  = "rs1"
   storage_account_name = "strg1234"
-  container_name       = "Tcontainer"
+  container_name       = "tcontainer"
   key                  = "storagestate.tfstate"
   }
 }
