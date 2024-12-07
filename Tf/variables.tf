f82a2391a7746d75926d611f79ac4cc0cd32baca
anu erics <anuerics@outlook.com>
2024-12-07 05:26:17 +0000

    Updated variables.tf

--- Tf/variables.tf
+++ Tf/variables.tf
@@ -20,22 +20,22 @@ variable "environment" {
   default     = "dev"
 }
 
-variable "client_id" {
-  description = "The name of the resource group"
-  type        = string
-}
+// variable "client_id" {
+//   description = "The name of the resource group"
+//   type        = string
+// }
 
-variable "tenant_id" {
-  description = "The name of the resource group"
-  type        = string
-}
+// variable "tenant_id" {
+//   description = "The name of the resource group"
+//   type        = string
+// }
 
-variable "client_secret" {
-  description = "The name of the resource group"
-  type        = string
-}
+// variable "client_secret" {
+//   description = "The name of the resource group"
+//   type        = string
+// }
 
-variable "subscription_id" {
-  description = "The name of the resource group"
-  type        = string
-}
+// variable "subscription_id" {
+//   description = "The name of the resource group"
+//   type        = string
+// }
