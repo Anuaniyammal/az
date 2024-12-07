6704762e882fdebbaf2a999b1f3a8ccb4703e16e
anu erics <anuerics@outlook.com>
2024-12-07 06:56:52 +0000

    Deleted backend.tf

--- Tf/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-  resource_group_name  = "rs1"
-  storage_account_name = "strg1234"
-  container_name       = "tcontainer"
-  key                  = "storagestate.tfstate"
-  }
-}
