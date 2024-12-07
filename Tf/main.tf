2086cbe6c25f504d3920100667be326669b7c670
anu erics <anuerics@outlook.com>
2024-12-07 04:34:23 +0000

    Updated main.tf

--- Tf/main.tf
+++ Tf/main.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "example" {
+data "azurerm_resource_group" "example" {
   name     = var.resource_group_name
-  location = var.location
+  #location = var.location
 }
 
 resource "azurerm_storage_account" "example" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = data.azurerm_resource_group.example.name
+  location                 = data.azurerm_resource_group.example.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
